0cb0e28d2aa7be87e8a6ecff22edb4036772a616
Florian Tournay <florian.tournay@hotmail.fr>
2025-02-17 17:57:45 +0100

    Add Base CSS & homepage

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../core/components/header/header.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
