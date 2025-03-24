c51c675ef06b729ae03818dd72f2d9e20deb2101
Florian Tournay <florian.tournay@hotmail.fr>
2025-03-24 16:53:57 +0100

    Optimize existing interfaces / Move some interfaces into utilities file

--- src/app/core/models/monsterDex.type.ts
+++ src/app/core/models/monsterDex.type.ts
@@ -1,51 +1,17 @@
+import { Description, Name, NamedAPIResource } from "./monsterUtilities.type"
 
-export type Description = {
-  description: string,
-  langague: {
-    name: string,
-    url: string
-  }
+export interface PokemonEntry {
+  entry_number: number;
+  pokemon_species: NamedAPIResource;
 }
 
-export type Name = {
-  language: {
-    name: string,
-    url: string
-  },
-  name: string
-}
-
-export type PokemonEntry = {
-  entry_number: number,
-  pokemon_species:{
-    name: string,
-    url: string
-  }
-}
-
-export type VersionGroup = {
-  name: string,
-  url: string
-}
-
-export type Pokedex = {
-  descriptions: [
-    Description
-  ],
-  id: number,
-  is_main_series: boolean,
-  name: string,
-  names: [
-    Name
-  ],
-  pokemon_entries: [
-    PokemonEntry
-  ],
-  region: {
-    name: string,
-    url: string
-  },
-  version_groups: [
-    VersionGroup
-  ]
+export interface Pokedex {
+  descriptions: Description[];
+  id: number;
+  is_main_series: boolean;
+  name: string;
+  names: Name[];
+  pokemon_entries: PokemonEntry[];
+  region: NamedAPIResource;
+  version_groups: NamedAPIResource[];
 }
--- src/app/core/models/monsterUtilities.type.ts
+++ src/app/core/models/monsterUtilities.type.ts
@@ -1,9 +1,25 @@
+export interface Description {
+  description: string;
+  langague: {
+    name: string;
+    url: string;
+  }
+}
+
+export interface Name {
+  language: {
+    name: string;
+    url: string;
+  };
+  name: string;
+}
+
 export interface NamedAPIResource {
   name: string;
   url: string;
 }
 
 export interface VersionGameIndex {
-  game_index: number,
-  version: NamedAPIResource
+  game_index: number;
+  version: NamedAPIResource;
 }
