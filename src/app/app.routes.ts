890541ced49dc94d336c19a5e52bdf585a56bbdc
Florian Tournay <florian.tournay@hotmail.fr>
2025-03-28 03:33:24 +0100

    Move pokedex entry to its own component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MonsterPageComponent } from './monster-page/monster-page.component';
-import { MonsterDexListComponent } from './monster-dex-list/monster-dex-list.component';
+import { MonsterDexListPageComponent } from './monster-dex-list/monster-dex-list-page/monster-dex-list-page.component';
 import { MonsterListComponent } from './monster-list/monster-list.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'pokedexes', component: MonsterDexListComponent},
+  { path: 'pokedexes', component: MonsterDexListPageComponent},
   { path: 'pokedex/:region', component: MonsterListComponent},
   { path: 'pokemon/:enMonsterName', component: MonsterPageComponent}
 ];
