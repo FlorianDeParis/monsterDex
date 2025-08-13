ff91680ce14b86b561966877e17c2663502c74e3
Florian Tournay <florian.tournay@hotmail.fr>
2025-08-14 01:39:24 +0200

    Clean up types

--- src/app/core/services/poke-api.service.ts
+++ src/app/core/services/poke-api.service.ts
@@ -2,7 +2,7 @@ import { environment } from '../env/environment';
 import { Pokedex } from '../models/PokeAPI/games.type';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { Pokemon } from '../models/PokeAPI/pokemon.type';
 
 @Injectable({
