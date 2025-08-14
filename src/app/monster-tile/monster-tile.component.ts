37ef32e030c5d8aff6334c63092ee8980f5383b0
Florian Tournay <ftournay@idobs.net>
2025-08-15 01:35:59 +0200

    WIP - try to assign poke gen on tile

--- src/app/monster-tile/monster-tile.component.ts
+++ src/app/monster-tile/monster-tile.component.ts
@@ -1,5 +1,5 @@
 import { MonsterService } from './../core/services/monster.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 import { PokemonEntry } from '../core/models/PokeAPI/games.type';
 import { CommonModule } from '@angular/common';
 import { environment } from '../core/env/environment';
@@ -13,18 +13,22 @@ import { preventTrailingSlashes } from '../core/utils/url';
   styleUrl: './monster-tile.component.scss'
 })
 export class MonsterTileComponent implements OnInit {
-
+  @HostBinding('pokedexId') pokedexId: any;
   @Input() pokemon!: PokemonEntry;
   @Input() idDex!: number;
+  pokemonGeneration!: number | null;
   imageUrl!: string;
 
-  constructor(private router: Router, private MonsterService: MonsterService) {}
+  constructor(private router: Router, private monsterService: MonsterService) {
+    this.pokemonGeneration = this.monsterService.getPokedexPokemonGeneration(this.idDex);
+    console.log(this.pokedexId);
+  }
 
   ngOnInit(): void {
-    // this.imageUrl = `${environment.SPRITE_URL}/pokemon/${this.getIdMonster(this.pokemon.pokemon_species.url)}.png`;
+    this.imageUrl = `${environment.SPRITE_URL}/pokemon/${this.getIdMonster(this.pokemon.pokemon_species.url)}.png`;
     // this.imageUrl = this.MonsterService.getPokemonArtworkByIdGeneration(this.getIdMonster(this.pokemon.pokemon_species.url), this.idDex)
     // console.log(this.getIdMonster(this.pokemon.pokemon_species.url), this.idDex);
-    this.MonsterService.getPokemonArtworkByIdGeneration(this.getIdMonster(this.pokemon.pokemon_species.url), this.idDex);
+    //this.MonsterService.getPokemonArtworkByIdGeneration(this.getIdMonster(this.pokemon.pokemon_species.url), this.idDex);
   }
 
   getIdMonster(url: string): number{
@@ -32,7 +36,7 @@ export class MonsterTileComponent implements OnInit {
     return parseInt(monsterUrl[monsterUrl.length - 1]);
   }
 
-  goToMonsterPage(monsterName: string): void {
-    this.router.navigateByUrl(`/pokemon/${this.idDex}/${monsterName}`)
+  goToMonsterPage(idMonster: string): void {
+    this.router.navigateByUrl(`/pokemon/${idMonster}/${this.pokemonGeneration}/${this.idDex}`)
   }
 }
