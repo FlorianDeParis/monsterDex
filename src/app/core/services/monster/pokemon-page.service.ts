c372902c9547aec2489664b311f821c47384c0cf
Florian Tournay <ftournay@idobs.net>
2025-08-21 01:50:22 +0200

    WIP SPRITE INDEX

--- src/app/core/services/monster/pokemon-page.service.ts
+++ src/app/core/services/monster/pokemon-page.service.ts
@@ -1,27 +1,27 @@
 import { PokeApiService } from './../poke-api.service';
 import { Injectable } from '@angular/core';
 import { toRoman } from "typescript-roman-numbers-converter";
+import { PokemonSprites } from '../../models/PokeAPI/pokemon.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonPageService {
 
-  constructor(private pokeApiService: PokeApiService ) { }
+  constructor(private pokeApiService: PokeApiService) { }
 
   // TO DO
   // getPokemonDetailsByIdDex(monsterName: string, idDex: number){
 
   // }
 
-  // getPokemonArtworkByIdGeneration$(monsterId: number, IdGen: number){
-  //   console.log('get PKMN art');
-  //   let test = this.pokeApiService.getPokemonDetails(monsterId).pipe(
-  //     map((pokemonEntry) => pokemonEntry.sprites),
-  //     // tap((data) => console.log(data)),
-  //     //map(spriteObj => spriteObj)
-  //   ).subscribe();
-  // }
+  getPokemonArtworkByIdGeneration(spriteObject: PokemonSprites, IdGen: string){
+    const romanGeneration = toRoman(parseInt(IdGen)).toLowerCase();
+    const genKey = `generation-${romanGeneration}`;
+    console.log(spriteObject, IdGen, romanGeneration);
+    const gentObj = (spriteObject.versions?[genKey][0].front_default) || spriteObject.front_default;
+    console.log(spriteObject.versions?.[genKey]);
+  }
 
   // filterSpriteObjectByDexGen$(spriteObj:PokemonSprites, IdGen:number){
   //   //
