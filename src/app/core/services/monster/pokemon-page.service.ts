7cac058082c5d37c169152b4f5f551db2f805217
Florian Tournay <ftournay@idobs.net>
2025-08-28 00:23:43 +0200

    Manage sprite exceptions

--- src/app/core/services/monster/pokemon-page.service.ts
+++ src/app/core/services/monster/pokemon-page.service.ts
@@ -2,13 +2,17 @@ import { PokeApiService } from './../poke-api.service';
 import { Injectable } from '@angular/core';
 import { toRoman } from "typescript-roman-numbers-converter";
 import { PokemonSprites, PokemonSpritesVersions } from '../../models/PokeAPI/pokemon.type';
+import { ToasterService } from '../toaster.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonPageService {
 
-  constructor(private pokeApiService: PokeApiService) { }
+  constructor(
+    private pokeApiService: PokeApiService,
+    private toaster: ToasterService
+  ) { }
 
   // TO DO
   // getPokemonDetailsByIdDex(monsterName: string, idDex: number){
@@ -23,12 +27,18 @@ export class PokemonPageService {
       const generationKey = ('generation-'+romanGeneration);
       let spriteWithGen = this.getMyObjectValueCastedKey(spriteObject.versions, generationKey);
       if(spriteWithGen){
-        spriteFront = this.getMyObjectValueCastedKey(spriteWithGen[Object.keys(spriteWithGen)[0]], 'front_default');
+        const indexesWithoutIcons = Object.keys(spriteWithGen).filter((index) => index != "icons"); // Ignore icon object when fetching sprites
+        if(indexesWithoutIcons.length > 0){
+          spriteFront = this.getMyObjectValueCastedKey(spriteWithGen[indexesWithoutIcons[0]], 'front_default');
+        }
       }
       if(spriteWithGen && spriteFront){
         returnedSprite = spriteFront;
+      } else {
+        this.toaster.error('Sprite non trouvé, chargement du sprite par défaut');
       }
     }
+    console.log('returnedSprite', returnedSprite);
     return returnedSprite as string;
   }
 
