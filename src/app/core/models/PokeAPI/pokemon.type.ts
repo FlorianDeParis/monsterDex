8d1ee0c8e88b4d48018dc02601d4769306c229f0
Florian Tournay <ftournay@idobs.net>
2026-02-04 23:58:19 +0100

    Add PokemonSpecies typing

--- src/app/core/models/PokeAPI/pokemon.type.ts
+++ src/app/core/models/PokeAPI/pokemon.type.ts
@@ -1,4 +1,8 @@
 import {
+  APIResource,
+  Description,
+  FlavorText,
+  Name,
   NamedAPIResource,
   VersionEncounterDetail,
   VersionGameIndex,
@@ -283,4 +287,55 @@ export interface PokemonStat {
 export interface LocationAreaEncounter {
   location_area: NamedAPIResource;
   version_details: VersionEncounterDetail[];
+}
+
+export interface PokemonSpecies {
+  id: number;
+  name: string;
+  order: number;
+  gender_rate: number;
+  capture_rate: number;
+  base_happiness: number;
+  is_baby: boolean;
+  is_legendary: boolean;
+  is_mythical: boolean;
+  hatch_counter: number;
+  has_gender_differences: boolean;
+  forms_switchable: boolean;
+  growth_rate: NamedAPIResource;
+  pokedex_numbers: PokemonSpeciesDexEntry[];
+  egg_groups: NamedAPIResource[];
+  color: NamedAPIResource;
+  shape: NamedAPIResource;
+  evolves_from_species: NamedAPIResource;
+  evolution_chain: APIResource;
+  habitat: NamedAPIResource;
+  generation: NamedAPIResource;
+  names: Name[];
+  pal_park_encounters: PalParkEncounterArea[];
+  flavor_text_entries: FlavorText[];
+  form_descriptions: Description[];
+  genera: Genus[];
+  varieties: PokemonSpeciesVariety[];
+}
+
+export interface Genus {
+  genus: string;
+  language: NamedAPIResource;
+}
+
+export interface PokemonSpeciesDexEntry {
+  entry_number: number;
+  pokedex: NamedAPIResource;
+}
+
+export interface PalParkEncounterArea {
+  base_score: number;
+  rate: number;
+  area: NamedAPIResource;
+}
+
+export interface PokemonSpeciesVariety {
+  is_default: boolean;
+  pokemon: NamedAPIResource;
 }
