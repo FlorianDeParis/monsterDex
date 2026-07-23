845454599795ee7c03ac0e10a33a8e9390b7e24c
Florian <florian.tournay@hotmail.fr>
2026-07-23 14:43:47 +0200

    Add filter engine

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeaderService } from '../../services/components/header.service';
-import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [AsyncPipe],
+  imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   logoPath: string = 'assets/img/logo.png';
   showSearch$!: Observable<boolean>;
+  search = new FormControl('');
 
 
   constructor(
@@ -20,6 +22,24 @@ export class HeaderComponent {
     private router: Router
   ){
     this.showSearch$ = this.header.showSearch$;
+  }
+
+  ngOnInit(): void {
+    if(this.showSearch$){
+      this.search.valueChanges.pipe(
+        debounceTime(500),
+        distinctUntilChanged()
+      ).subscribe(
+        (value) => {
+          console.log("value change "+value);
+          if(!!value){
+            this.header.setSearchQuery(value);
+          } else {
+            this.header.resetSearchQuery();
+          }
+        }
+      )
+    }
   }
 
   goToHomePage(): void {
